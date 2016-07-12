90ca012b14e008868b77dfc608a8639221ae64a9
anastazie <anastazie.d@gmail.com>
2016-07-12 12:03:08 +0200

    .gitignore updated. more examples added to word2vec_model_test.py

--- word2vec_model_test.py
+++ word2vec_model_test.py
@@ -1,21 +1,44 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jul 12 07:25:19 2016
+Test created model with Czech words
 
-@author: nastya
+Example:
+>>> model.most_similar(positive=['žena', 'král'], negative=['muž']) 
+[('královna', 0.5616521239280701),
+ ('manželka', 0.5222904682159424),
+ ('alžběta', 0.5171604156494141),
+ ('choť', 0.5107160806655884),
+ ('císařovna', 0.5097413659095764),
+ ('isabela', 0.49988824129104614),
+ ('konstancie', 0.48815155029296875),
+ ('regentka', 0.48672398924827576),
+ ('eleonora', 0.48375043272972107),
+ ('kněžna', 0.47714075446128845)]
+
 """
-from gensim.models import Word2Vec
-model = Word2Vec.load('~/wiki/wiki.cz.model')
-model.most_similar('žena', topn=5)
 
-model.most_similar('muž', topn=5)
+from gensim.models import Word2Vec
+# Load model
+model = Word2Vec.load('/home/nasta/nlp_czech_wiki/wiki.cz.model')
+
+model.most_similar(positive=['žena', 'král'], negative=['muž'])
+model.most_similar(positive=['prachy', 'pohoda'], negative=['práce'])
+
+# Slovak words are also present
+model.most_similar('pieseň', topn=5)
+
+# Words need to be stemmed
+model.most_similar('strom', topn=5)
+
+# Catch rare words
+model.most_similar('lokna', topn=5)
 
 model.most_similar('seznam', topn=5)
 
 model.similarity('žena', 'muž')
 model.similarity('žena', 'dívka')
+model.similarity('muž', 'mladík')
 
 model.doesnt_match("oběd snídaně kočka svačina".split())
 model.doesnt_match("oběd snídaně hruška svačina".split())
 
-model.most_similar(positive=['žena', 'král'], negative=['muž'])
